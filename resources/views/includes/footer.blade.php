fb86ded1e7aff423ca50aefb2e1c2e8738864b4b
Modi934 <kabilm934@gmail.com>
2025-02-05 18:46:00 +0200

    Add files via upload

---
+++ resources/views/includes/footer.blade.php
@@ -0,0 +1,11 @@
+<div class="footer">
+    <div class="container">
+        <div class="footer-text">
+            <p>
+                 Design by eng: " mohammed kabil __ yousef abdelaziz "
+               
+                </a>
+            </p>
+        </div>
+    </div>
+</div>
